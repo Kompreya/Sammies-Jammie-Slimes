e38ec5419b266b957017b3f2d8adff41c23732f7
Kompy <41587427+Kompreya@users.noreply.github.com>
2023-09-18 14:45:53 -0400

    entityID validation in parse
    Wrapped the entityID validation process into the json parser

--- src/main/java/kombee/sammiejamslimes/JSONFileLoader.java
+++ src/main/java/kombee/sammiejamslimes/JSONFileLoader.java
@@ -1,17 +1,10 @@
 package kombee.sammiejamslimes;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
+import java.io.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -20,64 +13,111 @@ public class JSONFileLoader {
     private static final JsonParser jsonParser = new JsonParser();
     private static final Logger LOGGER = LogManager.getLogger(SammieJamSlimes.MODID);
 
+    // SoC: Parsing and validating slimes.json before storing in SammieJamSlimeData
+    // Here we perform a grand manner of validation checks on slimes.json.
+    // Hard reject or expect critical value types and formatting, throw error indicating expected correct formatting or type.
+    // Soft accept some values, sanitize as needed, validate and store.
+    // Prevent crashes when possible. Provide fallbacks. Throw warnings indicating fallback used.
     public static SammieJamSlimeData[] loadSlimeData(String filePath, boolean loadDefaultOnFail) {
-        try {
+        Set<String> encounteredEntityIDs = new HashSet<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             FileReader fileReader = new FileReader(filePath);
+            JsonArray jsonArray = new JsonArray();
+            String line;
+            int lineNumber = 0;
+            String currentEntityID = null; // Track the current entityID. We do this, so we can associate properties to entityID, as entityID is required for each slime object.
 
-            // Check if the file is empty (zero bytes)
-            if (fileReader.ready()) {
-                JsonArray jsonArray = jsonParser.parse(fileReader).getAsJsonArray();
+            // Track line number for use in logging.
+            while ((line = reader.readLine()) != null) {
+                lineNumber++;
 
-                SammieJamSlimeData[] slimeDataArray = new SammieJamSlimeData[jsonArray.size()];
-                Set<String> encounteredEntityIDs = new HashSet<>(); // Maintain encountered entityIDs
-
-                int i = 0;
-                for (JsonElement element : jsonArray) {
-                    SammieJamSlimeData slimeData = gson.fromJson(element, SammieJamSlimeData.class);
-
-                    // Check if the entityID property has an error and log it
-                    if (slimeData.hasEntityIDError()) {
-                        LOGGER.error("Invalid entityID found in slimes.json: {}", element);
-                    }
-
-                    // Check for duplicate entityID
-                    if (!encounteredEntityIDs.add(slimeData.getEntityID())) {
-                        LOGGER.error("Duplicate entityID found in slimes.json: {}. Will not be registered. Ensure entityID values are unique.", slimeData.getEntityID());
-                    }
-
-                    // Add the slimeData object to your array
-                    slimeDataArray[i] = slimeData;
-                    i++;
+                // Skip empty lines or lines with only whitespace
+                if (line.trim().isEmpty()) {
+                    continue;
                 }
 
-                return slimeDataArray;
+                try {
+                    // Attempt to parse each line as JSON
+                    JsonElement jsonElement = jsonParser.parse(line);
+
+                    if (jsonElement.isJsonObject()) {
+                        // Check if the JSON object has an "entityID" property
+                        JsonObject jsonObject = jsonElement.getAsJsonObject();
+                        JsonElement entityIDElement = jsonObject.get("entityID");
+
+                        if (entityIDElement != null && entityIDElement.isJsonPrimitive() && entityIDElement.getAsString() != null) {
+                            currentEntityID = entityIDElement.getAsString();
+
+                            // Validate the entityID format
+                            if (!isValidEntityID(currentEntityID)) {
+                                LOGGER.error("Line {}: Invalid entityID format for '{}': {}. Skipping.", lineNumber, currentEntityID, line);
+                                continue; // Skip invalid entityID
+                            }
+                        } else {
+                            currentEntityID = null;
+                            LOGGER.warn("Line {}: JSON object missing or has an invalid 'entityID' property. Skipping.", lineNumber);
+                            continue;
+                        }
+
+                        jsonArray.add(jsonObject);
+                    } else {
+                        LOGGER.warn("Line {}: Ignored invalid JSON data: {}", lineNumber, line);
+                    }
+                } catch (JsonParseException e) {
+                    LOGGER.warn("Line {}: Ignored invalid JSON data: {}", lineNumber, line);
+                }
             }
+
+            SammieJamSlimeData[] slimeDataArray = new SammieJamSlimeData[jsonArray.size()];
+
+            int i = 0;
+            for (JsonElement element : jsonArray) {
+                SammieJamSlimeData slimeData = gson.fromJson(element, SammieJamSlimeData.class);
+
+                // Check if the entityID property has an error and log it
+                if (slimeData.hasEntityIDError()) {
+                    LOGGER.error("EntityID validation error for '{}': {}", slimeData.getEntityID(), element);
+                }
+
+                // Add the slimeData object to your array
+                slimeDataArray[i] = slimeData;
+                encounteredEntityIDs.add(slimeData.getEntityID());
+                i++;
+            }
+
+            return slimeDataArray;
         } catch (IOException e) {
             e.printStackTrace();
             if (loadDefaultOnFail) {
-                // Load the default data from the mod's resources
-                try (InputStream resourceStream = JSONFileLoader.class.getResourceAsStream("/assets/sammiejamslimes/slimes.json")) {
-                    if (resourceStream != null) {
-                        InputStreamReader reader = new InputStreamReader(resourceStream, StandardCharsets.UTF_8);
-                        JsonArray jsonArray = jsonParser.parse(reader).getAsJsonArray();
-
-                        SammieJamSlimeData[] slimeDataArray = new SammieJamSlimeData[jsonArray.size()];
-
-                        int i = 0;
-                        for (JsonElement element : jsonArray) {
-                            slimeDataArray[i] = gson.fromJson(element, SammieJamSlimeData.class);
-                            i++;
-                        }
-
-                        return slimeDataArray;
-                    }
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
+                // Load the default data from the mod's resources folder. I provided a default slimes.json
             }
         }
-
         return null; // Return null if the file is empty or there was an IO error.
     }
 
+    // Place json validation methods and their respective validation conditions here.
+    //
+
+    // Validate entityID format
+    private static boolean isValidEntityID(String entityID) {
+        if (entityID == null || entityID.isEmpty()) {
+            return false;
+        }
+
+        // Replace spaces with underscores and convert to lowercase
+        String sanitizedEntityID = entityID.replaceAll(" ", "_").toLowerCase();
+
+        // Check if the sanitized entityID matches the desired format: lowercase, alphanumeric, underscore
+        if (sanitizedEntityID.matches("[a-z0-9_]+")) {
+            // Check if the original entityID contained uppercase characters or spaces
+            if (!entityID.equals(sanitizedEntityID)) {
+                // Warn the user about the sanitization
+                LOGGER.warn("EntityID '{}' contains uppercase characters or spaces. Converted to '{}'.", entityID, sanitizedEntityID);
+            }
+            return true;
+        } else {
+            // If the sanitized entityID doesn't match the format, it's invalid
+            return false;
+        }
+    }
 }
--- src/main/java/kombee/sammiejamslimes/SammieJamSlimeData.java
+++ src/main/java/kombee/sammiejamslimes/SammieJamSlimeData.java
@@ -1,5 +1,7 @@
 package kombee.sammiejamslimes;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -23,6 +25,7 @@ public class SammieJamSlimeData {
         return entityID;
     }
 
+    // Currently unused. Validation is being performed during parse. Modify this later based on needs of code handling entity IDs
     public void setEntityID(String entityID) {
         // Validate the entityID before setting it
         if (isValidEntityID(entityID)) {
@@ -38,6 +41,7 @@ public class SammieJamSlimeData {
         return displayName;
     }
 
+    // Same as entityID. Revisit later. Validation occurs in parse.
     public void setDisplayName(String displayName) {
         // Validate displayName to ensure it's not empty
         if (displayName != null && !displayName.isEmpty()) {
@@ -52,6 +56,21 @@ public class SammieJamSlimeData {
     public SpawnEggColors getSpawnEggColors() {
         return spawnEggColors;
     }
+
+    public void setSpawnEggColors(JsonObject spawnEggColorsJson) {
+        // Validate the spawnEggColorsJson object
+        if (isValidSpawnEggColorsJson(spawnEggColorsJson)) {
+            // Extract and set the primary and secondary colors
+            this.spawnEggColors = new SpawnEggColors(
+                    spawnEggColorsJson.get("primary").getAsString(),
+                    spawnEggColorsJson.get("secondary").getAsString()
+            );
+        } else {
+            // Set default colors or handle the error as needed
+            this.spawnEggColors = new SpawnEggColors("FF0000", "00FF00");
+        }
+    }
+
 
     public List<TransformItem> getTransformItems() {
         return transformItems;
@@ -73,6 +92,12 @@ public class SammieJamSlimeData {
         private String primary;
         private String secondary;
 
+        // Constructor that accepts primary and secondary colors
+        public SpawnEggColors(String primary, String secondary) {
+            this.primary = primary;
+            this.secondary = secondary;
+        }
+
         public String getPrimary() {
             return primary;
         }
@@ -81,6 +106,7 @@ public class SammieJamSlimeData {
             return secondary;
         }
     }
+
 
     public static class TransformItem {
         private String itemID;
@@ -135,7 +161,7 @@ public class SammieJamSlimeData {
     public boolean hasDisplayNameError() {
         return displayNameError;
     }
-    private boolean isValidEntityID(String entityID) {
+    public boolean isValidEntityID(String entityID) {
         // Check if the entityID is not null and not empty
         if (entityID == null || entityID.isEmpty()) {
             return false;
@@ -143,6 +169,45 @@ public class SammieJamSlimeData {
 
         // Checking for entityID formatting. Accepts lowercase, alphanumeric, underscore.
         return entityID.matches("[a-z0-9_]");
+    }
+
+    private boolean isValidSpawnEggColorsJson(JsonObject spawnEggColorsJson) {
+        // Check if spawnEggColorsJson is not null
+        if (spawnEggColorsJson == null) {
+            return false;
+        }
+
+        // Check if "primary" and "secondary" properties exist and have valid formats
+        JsonElement primaryElement = spawnEggColorsJson.get("primary");
+        JsonElement secondaryElement = spawnEggColorsJson.get("secondary");
+
+        if (primaryElement == null || !primaryElement.isJsonPrimitive() || !isValidColor(primaryElement.getAsString())) {
+            return false;
+        }
+
+        if (secondaryElement == null || !secondaryElement.isJsonPrimitive() || !isValidColor(secondaryElement.getAsString())) {
+            return false;
+        }
+
+        // Check if "primary" color is empty
+        if (primaryElement.getAsString().isEmpty()) {
+            LOGGER.warn("The 'primary' color is empty in spawnEggColors. Using default primary color.");
+        }
+
+        // Check if "secondary" color is empty
+        if (secondaryElement.getAsString().isEmpty()) {
+            LOGGER.warn("The 'secondary' color is empty in spawnEggColors. Using default secondary color.");
+        }
+
+        return true;
+    }
+
+    private boolean isValidColor(String color) {
+        // You can implement your color format validation logic here
+        // For example, check if the color is a valid hexadecimal RGB code
+        // This depends on your specific requirements
+        // Here's a basic example:
+        return color.matches("^([A-Fa-f0-9]{6}|[A-Fa-f0-9]{8})$");
     }
 
 }
